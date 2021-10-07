21e9af80595a9f01303b00db4a58a05be7e75679
FrederST <freder.hernandez@correounivalle.edu.co>
2021-10-07 17:49:42 -0500

    fix: Add return type in MessagingServiceProvider class

--- src/Providers/MessagingServiceProvider.php
+++ src/Providers/MessagingServiceProvider.php
@@ -7,27 +7,27 @@ use Illuminate\Support\ServiceProvider;
 class MessagingServiceProvider extends ServiceProvider
 {
 
-    public function boot()
+    public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->registerPublishing();
         }
     }
 
-    public function register()
+    public function register(): void
     {
         $this->registerResources();
     }
 
 
-    protected function registerPublishing()
+    protected function registerPublishing(): void
     {
         $this->publishes([
             __DIR__ . '/../../config/amqp.php' => config_path('amqp.php'),
         ], 'amqp-config');
     }
 
-    protected function registerResources()
+    protected function registerResources(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../../config/amqp.php', 'amqp');
     }
