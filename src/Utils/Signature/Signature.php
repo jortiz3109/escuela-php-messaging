89a39b765553b990abded20f4671000961f28b64
christian <christian@pleno.digital>
2021-10-12 15:33:05 -0500

    refactor: add Exception Class to signature verify when the format is wrong

--- src/Utils/Signature/Signature.php
+++ src/Utils/Signature/Signature.php
@@ -2,10 +2,11 @@
 
 namespace E4\Messaging\Utils\Signature;
 
-use E4\Messaging\Utils\Signature\Exceptions\MissingPKException;
+use E4\Messaging\Utils\Signature\Exceptions\SignatureException;
+use Throwable;
 
 /**
- * Clase que permite firmar y verificar un mensaje usando el algoritmo SHA256
+ * Clase que permite firmar y verificar un mensaje
  *
  * Comando para crear clave en Linux/MAC:
  * openssl ecparam -name secp256k1 -genkey -out privateKey.pem
@@ -19,41 +20,48 @@ class Signature
 
     public function __construct(int $algorithm, string $publicKey, string|null $privateKey = null)
     {
-        $this->algorithm   = $algorithm;
+        $this->algorithm  = $algorithm;
         $this->publicKey  = $publicKey;
         $this->privateKey = $privateKey;
     }
 
     /**
-     * Crea la firma digital en base64
+     * Crea la firma digital y la envía codificada en base64
      *
      * @param string $message
      *
      * @return string
-     * @throws MissingPKException
+     * @throws SignatureException
+     * @throws Exception if the private key is wrong
      */
     public function sign(string $message): string
     {
         if (!$this->privateKey){
-            throw new MissingPKException('Is necesary the private key');
+            throw new SignatureException('Is necessary the private key');
         }
-        if (!openssl_sign($message, $sign, $this->privateKey, $this->algorithm)) {
-            throw new MissingPKException('The correct algorithm is required');
+        if (!openssl_sign($message, $signature, $this->privateKey, $this->algorithm)) {
+            throw new SignatureException('The correct algorithm is required');
         }
-        return base64_encode($sign);
+        return base64_encode($signature);
     }
 
     /**
-     * Verifica que la firma digital sea correcta
+     * Verifica si una firma digital es correcta. Si la firma digital es erronea o el algoritmo de la firma no es
+     * compactible con el de la llave publica, genera excepción
      *
      * @param string $message
      * @param string $signatureInBase64
      *
      * @return bool
+     * @throws SignatureException
+     * @throws Exception if the public key is wrong
      */
     public function verify(string $message, string $signatureInBase64): bool
     {
-        $success = openssl_verify($message, base64_decode($signatureInBase64), $this->publicKey, $this->algorithm);
-        return ($success == 1);
+        $verified = openssl_verify($message, base64_decode($signatureInBase64), $this->publicKey, $this->algorithm);
+        if ($verified == -1) {
+            throw new SignatureException('The signature format or the signature algorithm is wrong');
+        }
+        return ($verified == 1);
     }
 }
--- tests/Unit/Utils/Signature/SignatureTest.php
+++ tests/Unit/Utils/Signature/SignatureTest.php
@@ -4,14 +4,15 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use E4\Messaging\Utils\Signature\Signature;
-use E4\Messaging\Utils\Signature\Exceptions\MissingPKException;
+use E4\Messaging\Utils\Signature\Exceptions\SignatureException;
+use Exception;
 
 class SignatureTest extends TestCase
 {
     private string $privateKey;
     private string $publicKey;
-    private static int $algorithm    = OPENSSL_ALGO_SHA256;
-    private static string $msgToSign = 'Mensaje a firmar';
+    private int    $algorithm    = OPENSSL_ALGO_SHA256;
+    private string $msgToSign = 'Mensaje a firmar';
 
     public function setUp(): void
     {
@@ -21,45 +22,54 @@ class SignatureTest extends TestCase
 
     public function test_it_shows_error_when_creating_signature_without_private_key(): void
     {
-        $this->expectException(MissingPKException::class);
-        $this->expectExceptionMessage('Is necesary the private key');
-        $signer = new Signature(self::$algorithm, $this->publicKey);
+        $this->expectException(SignatureException::class);
+        $this->expectExceptionMessage('Is necessary the private key');
+        $signer = new Signature($this->algorithm, $this->publicKey);
 
-        $signer->sign(self::$msgToSign);
+        $signer->sign($this->msgToSign);
     }
 
     public function test_it_shows_error_when_creating_signature_with_incorrect_algorithm(): void
     {
-        $this->expectException(MissingPKException::class);
+        $this->expectException(SignatureException::class);
         $this->expectExceptionMessage('The correct algorithm is required');
         $signer = new Signature(OPENSSL_ALGO_MD5, $this->publicKey, $this->privateKey);
 
-        $signer->sign(self::$msgToSign);
+        $signer->sign($this->msgToSign);
     }
 
     public function test_it_create_signature_correctly(): void
     {
-        $signer = new Signature(self::$algorithm, $this->publicKey, $this->privateKey);
-        $sign   = $signer->sign(self::$msgToSign);
+        $signer = new Signature($this->algorithm, $this->publicKey, $this->privateKey);
+        $sign   = $signer->sign('$this->msgToSign');
 
         $this->assertNotEmpty($sign);
     }
 
     public function test_it_return_false_when_the_signature_is_incorrect(): void
     {
-        $signer = new Signature(self::$algorithm, $this->publicKey);
-        $sign   = 'FIRMA_INCORRECTA';
-        $res    = $signer->verify(self::$msgToSign, $sign);
+        $signer = new Signature($this->algorithm, $this->publicKey);
+        $sign   = 'MEUCIQDEjlRMiAYyV0AsT0E9xtN7g2wZeWQO/mrfU5R85uEs6gIgN9/4dfpq4QG7kaOJ9s9Cpm74njKdJPB/O3MKeQgp0QI=';
+        $res    = $signer->verify($this->msgToSign, $sign);
 
         $this->assertFalse($res);
     }
 
     public function test_it_return_true_when_the_signature_is_correct(): void
     {
-        $signer = new Signature(self::$algorithm, $this->publicKey);
+        $signer = new Signature($this->algorithm, $this->publicKey);
         $sign   = 'MEYCIQC9vTCpwef4JaYcb1ub2Mpk1aUMo4eqEoC1jSa9ixll9gIhAPc2K7W8j3vl3AD73XItdQrdCUf970WkSIKrEAi0Fhvn';
-        $res    = $signer->verify(self::$msgToSign, $sign);
+        $res    = $signer->verify($this->msgToSign, $sign);
 
         $this->assertTrue($res);
+    }
+
+    public function test_it_shows_error_when_validating_the_signature_with_wrong_algorithm(): void
+    {
+        $this->expectException(SignatureException::class);
+        $signer = new Signature(OPENSSL_ALGO_MD5, $this->publicKey);
+        $sign   = 'MEUCIQDEjlRMiAYyV0AsT0E9xtN7g2wZeWQO/mrfU5R85uEs6gIgN9/4dfpq4QG7kaOJ9s9Cpm74njKdJPB/O3MKeQgp0QI=';
+
+        $signer->verify($this->msgToSign, $sign);
     }
 }
