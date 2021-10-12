098299944e9705b82d33a3d1b4736a6e905e1d46
christian <christian@pleno.digital>
2021-10-12 12:24:51 -0500

    refactor: change static vars in unit test class

--- tests/Unit/Utils/Signature/SignatureTest.php
+++ tests/Unit/Utils/Signature/SignatureTest.php
@@ -10,23 +10,22 @@ class SignatureTest extends TestCase
 {
     private string $privateKey;
     private string $publicKey;
-    private string $algorithm;
-    private string $msgToSign = 'Mensaje a firmar';
+    private static int $algorithm    = OPENSSL_ALGO_SHA256;
+    private static string $msgToSign = 'Mensaje a firmar';
 
     public function setUp(): void
     {
         $this->privateKey = file_get_contents(__DIR__ . '/privateKey.pem');
         $this->publicKey  = file_get_contents(__DIR__ . '/publicKey.pem');
-        $this->algorithm  = OPENSSL_ALGO_SHA256;
     }
 
     public function test_it_shows_error_when_creating_signature_without_private_key(): void
     {
         $this->expectException(MissingPKException::class);
         $this->expectExceptionMessage('Is necesary the private key');
-        $signer = new Signature($this->algorithm, $this->publicKey);
+        $signer = new Signature(self::$algorithm, $this->publicKey);
 
-        $signer->sign($this->msgToSign);
+        $signer->sign(self::$msgToSign);
     }
 
     public function test_it_shows_error_when_creating_signature_with_incorrect_algorithm(): void
@@ -35,31 +34,31 @@ class SignatureTest extends TestCase
         $this->expectExceptionMessage('The correct algorithm is required');
         $signer = new Signature(OPENSSL_ALGO_MD5, $this->publicKey, $this->privateKey);
 
-        $signer->sign($this->msgToSign);
+        $signer->sign(self::$msgToSign);
     }
 
     public function test_it_create_signature_correctly(): void
     {
-        $signer = new Signature($this->algorithm, $this->publicKey, $this->privateKey);
-        $sign   = $signer->sign($this->msgToSign);
+        $signer = new Signature(self::$algorithm, $this->publicKey, $this->privateKey);
+        $sign   = $signer->sign(self::$msgToSign);
 
         $this->assertNotEmpty($sign);
     }
 
     public function test_it_return_false_when_the_signature_is_incorrect(): void
     {
-        $signer = new Signature($this->algorithm, $this->publicKey);
+        $signer = new Signature(self::$algorithm, $this->publicKey);
         $sign   = 'FIRMA_INCORRECTA';
-        $res    = $signer->verify($this->msgToSign, $sign);
+        $res    = $signer->verify(self::$msgToSign, $sign);
 
         $this->assertFalse($res);
     }
 
     public function test_it_return_true_when_the_signature_is_correct(): void
     {
-        $signer = new Signature($this->algorithm, $this->publicKey);
+        $signer = new Signature(self::$algorithm, $this->publicKey);
         $sign   = 'MEYCIQC9vTCpwef4JaYcb1ub2Mpk1aUMo4eqEoC1jSa9ixll9gIhAPc2K7W8j3vl3AD73XItdQrdCUf970WkSIKrEAi0Fhvn';
-        $res    = $signer->verify($this->msgToSign, $sign);
+        $res    = $signer->verify(self::$msgToSign, $sign);
 
         $this->assertTrue($res);
     }
