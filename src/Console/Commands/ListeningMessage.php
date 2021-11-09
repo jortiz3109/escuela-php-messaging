d0a8a2ca0320efe48622183258cf3658ff97b039
RODRIGO GALLEGO <50471819+ragallegom@users.noreply.github.com>
2021-11-09 08:38:38 -0500

    add new validation command listen

--- src/Console/Commands/ListeningMessage.php
+++ src/Console/Commands/ListeningMessage.php
@@ -57,21 +57,26 @@ class ListeningMessage extends Command
 
     private function consumeProcess(AMQPMessage $message)
     {
-        $this->line('Data message');
-        $this->line('Message encrypted:');
-        $this->line($message->body);
-        $this->line('Message decrypted:');
-        $this->line(json_encode($this->messageSecurity->prepareMsgToReceive($message->body)));
-        $this->newLine();
+//        $this->line('Data message');
+//        $this->line('Message encrypted:');
+//        $this->line($message->body);
+//        $this->line('Message decrypted:');
+//        $this->line(json_encode($this->messageSecurity->prepareMsgToReceive($message->body)));
+//        $this->newLine();
 
         $this->line('Dispatch event:');
         $events =  config('messagingapp.events');
         $this->line('Event: ' . $message->getRoutingKey());
         $this->newLine();
 
-        $this->line('Prepare MsgStructure:');
-        $data = $this->messageSecurity->prepareMsgToReceive($message->body);
-        $msg = new AMQPMessageStructure($message, $data);
+        try {
+            $this->line('Prepare MsgStructure:');
+            $data = $this->messageSecurity->prepareMsgToReceive($message->body);
+            $msg = new AMQPMessageStructure($message, $data);
+        } catch(\Exception $exception){
+            Log::error('Exception command messaging:listen ' . $exception);
+            $this->error('Exception: ' . $exception);
+        }
 
         if (array_key_exists($message->getRoutingKey(), $events)) {
             event(new $events[$message->getRoutingKey()]($msg));
