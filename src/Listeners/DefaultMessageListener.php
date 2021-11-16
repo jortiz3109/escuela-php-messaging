a8fc5320e33f64c5eaa955a7e1017c6b14b4ddfe
FrederST <freder.hernandez@correounivalle.edu.co>
2021-11-16 12:24:36 -0500

    fix: remove unnecessary comments

--- src/Listeners/DefaultMessageListener.php
+++ src/Listeners/DefaultMessageListener.php
@@ -8,12 +8,10 @@ class DefaultMessageListener
 {
     public function __construct()
     {
-        // Do nothing because is a listener.
     }
 
     public function handle(DefaultMessageEvent $event): void
     {
-//        print_r($event->message()); //  Exception: Memory exhaust
         $event->message()->ack();
     }
 }
